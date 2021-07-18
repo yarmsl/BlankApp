e869482791bfeb7a48087b11606a71ccfde0da56
Yaroslav <slideryo@gmail.com>
2021-07-18 13:17:02 +0500

    404 styled

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,27 @@
-import { Typography } from '@material-ui/core';
+import { Box, Button, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles(() => ({
+	root: {
+		height: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+	}
+}));
 
 const NotFound = () => {
+
+	const classes = useStyles();
+
 	return (
-		<>
-			<Typography>404</Typography>
-		</>
+		<Box className={classes.root}>
+			<Typography variant='h1'>404</Typography>
+			<Typography>Такой страницы пока нет, она появится в ближайшее время, а пока</Typography>
+			<Button component={ Link } to='/' color='primary'>вернитесь на главную</Button>
+		</Box>
 	);
 };
 
